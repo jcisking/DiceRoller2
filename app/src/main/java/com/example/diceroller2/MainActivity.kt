a13e7fbfeb36b4539f24e8bd756efcec590f91be
jcisking <krisloeffler@gmail.com>
2024-01-30 16:56:28 -0700

    added images and button

--- app/src/main/java/com/example/diceroller2/MainActivity.kt
+++ app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller2.ui.theme.DiceRoller2Theme
 
@@ -42,12 +44,16 @@ fun DiceRollerApp() {
 }
 
 
+
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(id = R.string.roll))
+        }
 
 
     }
